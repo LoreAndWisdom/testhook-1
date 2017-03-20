9d34a57390b6e4981c315f5541a6402e81a45aed
Lopo <lorenzo.lew@gmail.com>
2017-03-20 11:52:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// A simple hello world microservice 
+// A simple hello world microservice
 // Click "Deploy Service" to deploy this code
 // Service will respond to HTTP requests with a string
 module['exports'] = function helloWorld (hook) {
   // hook.req is a Node.js http.IncomingMessage
   var host = hook.req.host;
+  var data = hook.req.params;
+  console.log(params);
   // hook.res is a Node.js httpServer.ServerResponse
   // Respond to the request with a simple string
   hook.res.json(hook.params);
 };
+
