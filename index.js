9650396cb857016580f1da27eb352b9d40490a4e
Lopo <lorenzo.lew@gmail.com>
2017-03-20 11:52:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module['exports'] = function helloWorld (hook) {
   // hook.req is a Node.js http.IncomingMessage
   var host = hook.req.host;
   var data = hook.req.params;
-  console.log(params);
+  console.log(data);
   // hook.res is a Node.js httpServer.ServerResponse
   // Respond to the request with a simple string
   hook.res.json(hook.params);
